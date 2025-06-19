e389dcf630f2f3b9bf41fe84ce9b81ab80b651fa
SOROUSH911 <48121949+SOROUSH911@users.noreply.github.com>
2025-06-19 18:31:00 -0400

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome to AWS Amplify</h1>
+      <p>Your React app is deployed successfully!</p>
+    </div>
+  );
+}
+
+export default App;
